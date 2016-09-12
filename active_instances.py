fc4ff85973fc326a4b3dc93be7205ef76befbb15
Matthew Sanderson <matthew.sanderson@anu.edu.au>
2016-09-12 19:01:30 +1000

    Remove knowledge of the server's URL structure, replacing it with knowledge of the structure of the server's JSON responses, in an attempt to implement HATEOAS. This is still not correct, as there is no content negotiation and the content type is plain JSON, whereas it should be a well-defined content type based on JSON.

--- active_instances.py
+++ active_instances.py
@@ -26,48 +26,10 @@ def get_arg_or_env_var(args, name):
     return value
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Compile list of all active instances.'
-    )
-    parser.add_argument('--endpoint', required=True, help='reporting-api endpoint')
-    parser.add_argument('--output', required=True, help='output path')
-    parser.add_argument('--token', default=argparse.SUPPRESS, help='auth token for reporting-api')
-    parser.add_argument('--cache', default=False, action='store_true', help='cache results (for development)')
-    parser.add_argument('--debug', default=False, action='store_true', help='enable debug output (for development)')
-    parser.add_argument('--os-username', default=argparse.SUPPRESS, help='Username')
-    parser.add_argument('--os-password', default=argparse.SUPPRESS, help="User's password")
-    parser.add_argument('--os-auth-url', default=argparse.SUPPRESS, help='Authentication URL')
-    parser.add_argument('--os-project-name', default=argparse.SUPPRESS, help='Project name to scope to')
-    parser.add_argument('--os-tenant-name', default=argparse.SUPPRESS, help='Project name to scope to')
-    args = parser.parse_args()
-
-    args.token = get_arg_or_env_var(args, 'token')
-
-    if args.token is None:
-        # Attempt to obtain authentication credentials
-        username = get_arg_or_env_var(args, 'username')
-        password = get_arg_or_env_var(args, 'password')
-        project_name = get_arg_or_env_var(args, 'project_name')
-        if not project_name:
-            project_name = get_arg_or_env_var(args, 'tenant_name')
-        auth_url = get_arg_or_env_var(args, 'auth_url')
-        if username and password and project_name and auth_url:
-            keystone = keystone_client.Client(
-                username=username,
-                password=password,
-                project_name=project_name,
-                auth_url=auth_url
-            )
-            if not keystone.authenticate():
-                raise ValueError("Keystone authentication failed")
-            args.token = keystone.auth_ref['token']['id']
-
-    client = ReportingClient(**vars(args))
-
+def active_instances_csv(client, args):
     # grab all the required data
     hypervisor = client.fetch_w('hypervisor')
-    instance = client.fetch_w('instance?active=1')
+    instance = client.fetch_w('instance', active=1)
     project = client.fetch_w('project')
 
     # check that every hypervisor has availability_zone defined
@@ -134,3 +96,47 @@ if __name__ == '__main__':
         writer.writeheader()
         for (iid, i) in instance_by_id.items():
             writer.writerow(i)
+
+def test_api(client):
+    print(client.fetch('aggregate'))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Compile list of all active instances.'
+    )
+    parser.add_argument('--endpoint', required=True, help='reporting-api endpoint')
+    parser.add_argument('--output', required=True, help='output path')
+    parser.add_argument('--token', default=argparse.SUPPRESS, help='auth token for reporting-api')
+    parser.add_argument('--cache', default=False, action='store_true', help='cache results (for development)')
+    parser.add_argument('--debug', default=False, action='store_true', help='enable debug output (for development)')
+    parser.add_argument('--os-username', default=argparse.SUPPRESS, help='Username')
+    parser.add_argument('--os-password', default=argparse.SUPPRESS, help="User's password")
+    parser.add_argument('--os-auth-url', default=argparse.SUPPRESS, help='Authentication URL')
+    parser.add_argument('--os-project-name', default=argparse.SUPPRESS, help='Project name to scope to')
+    parser.add_argument('--os-tenant-name', default=argparse.SUPPRESS, help='Project name to scope to')
+    args = parser.parse_args()
+
+    args.token = get_arg_or_env_var(args, 'token')
+    if args.token is None:
+        # Attempt to obtain authentication credentials
+        username = get_arg_or_env_var(args, 'username')
+        password = get_arg_or_env_var(args, 'password')
+        project_name = get_arg_or_env_var(args, 'project_name')
+        if not project_name:
+            project_name = get_arg_or_env_var(args, 'tenant_name')
+        auth_url = get_arg_or_env_var(args, 'auth_url')
+        if username and password and project_name and auth_url:
+            keystone = keystone_client.Client(
+                username=username,
+                password=password,
+                project_name=project_name,
+                auth_url=auth_url
+            )
+            if not keystone.authenticate():
+                raise ValueError("Keystone authentication failed")
+            args.token = keystone.auth_ref['token']['id']
+
+    client = ReportingClient(**vars(args))
+
+    # sys.exit(test_api(client))
+    sys.exit(active_instances_csv(client, args))
--- reportingclient/client.py
+++ reportingclient/client.py
@@ -1,16 +1,62 @@
 import sys
 import json
 import requests
+from urllib import urlencode
 
 
 class ReportingClient(object):
+
     def __init__(self, endpoint, token=None, cache=False, debug=False, output=sys.stdout):
         self.token = token
         self.endpoint = endpoint
         self.cache = cache
         self.debug = debug
+        self.versions = None
+        self.reports = None
 
-    def fetch(self, report):
+    def _request(self, url, **params):
+        if self.endpoint.endswith('/') or url.startswith('/'):
+            url = self.endpoint + url
+        else:
+            url = self.endpoint + '/' + url
+        if len(params):
+            url = url + '?' + urlencode(params)
+        headers = {}
+        if self.token:
+            headers['X-Auth-Token'] = self.token
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+        return r.json()
+
+    def get_versions(self):
+        if not self.versions:
+            self.versions = self._request('')
+        return self.versions
+
+    def get_version(self, version_id):
+        for version in self.get_versions():
+            if version.id == version_id:
+                return version
+        raise ValueError("No server support for API version '" + version_id + "'")
+
+    def get_any_version_link(self, link_type):
+        for version in self.get_versions():
+            if link_type in version['links']:
+                return version['links'][link_type]
+        raise ValueError("No server API version supports link type '" + link_type + "'")
+
+    def get_reports(self):
+        if not self.reports:
+            self.reports = self._request(self.get_any_version_link('reports'))
+        return self.reports
+
+    def get_report_url(self, report_name):
+        for report in self.get_reports():
+            if report['name'] == report_name:
+                return report['links']['self']
+        raise ValueError("No report '" + report_name + "' available")
+            
+    def fetch(self, report, **params):
         """
         Fetch specified report from reporting-api endpoint, optionally passing given
         token as X-Auth-Token header.
@@ -22,13 +68,7 @@ class ReportingClient(object):
         repeatedly when having live data is not important.
         """
         def query_endpoint():
-            url = '{ep}/v1/reports/{re}'.format(ep=self.endpoint, re=report)
-            headers = {}
-            if self.token:
-                headers['X-Auth-Token'] = self.token
-            r = requests.get(url, headers=headers)
-            r.raise_for_status()
-            return r.json()
+            return self._request(self.get_report_url(report), **params)
     
         if self.cache:
             path = './{report}.json'.format(report=report)
@@ -43,7 +83,7 @@ class ReportingClient(object):
     
         return query_endpoint()
 
-    def fetch_w(self, report):
+    def fetch_w(self, report, **params):
         """
         Wrapper for `fetch`, incorporating commandline arguments and a little
         bit of error handling to make users' lives easier.
@@ -51,7 +91,7 @@ class ReportingClient(object):
         if self.debug:
             print 'Fetching "{}"...'.format(report)
         try:
-            data = self.fetch(report)
+            data = self.fetch(report, **params)
             if self.debug:
                 print 'Fetched "{}".'.format(report)
             return data
