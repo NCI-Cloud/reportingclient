226d9d4c4291e3b6ef5dff8a0d7ee3e0956c15ec
Matthew Sanderson <matthew.sanderson@anu.edu.au>
2016-09-12 16:39:53 +1000

    Move the methods that do URL accesses into a new ReportingClient class.

--- active_instances.py
+++ active_instances.py
@@ -8,38 +8,63 @@ import argparse
 import requests
 
 
-def fetch(report, endpoint, token=None, cache=False):
-    """
-    Fetch specified report from reporting-api endpoint, optionally passing given
-    token as X-Auth-Token header.
+class ReportingClient(object):
+    def __init__(self, endpoint, token=None, cache=False, debug=False, output=sys.stdout):
+        self.token = token
+        self.endpoint = endpoint
+        self.cache = cache
 
-    If `cache` is a truthy value, then instead of talking to the endpoint,
-    the data will be read from the file "./{report}.json". If that file does
-    not exist, it will be created and filled with data retrieved normally.
-    This is only to speed up development, avoiding the need to query endpoint
-    repeatedly when having live data is not important.
-    """
-    def query_endpoint():
-        url = '{ep}/v1/reports/{re}'.format(ep=endpoint, re=report)
-        headers = {}
-        if token:
-            headers['X-Auth-Token'] = token
-        r = requests.get(url, headers=headers)
-        r.raise_for_status()
-        return r.json()
+    def fetch(self, report):
+        """
+        Fetch specified report from reporting-api endpoint, optionally passing given
+        token as X-Auth-Token header.
+    
+        If `cache` is a truthy value, then instead of talking to the endpoint,
+        the data will be read from the file "./{report}.json". If that file does
+        not exist, it will be created and filled with data retrieved normally.
+        This is only to speed up development, avoiding the need to query endpoint
+        repeatedly when having live data is not important.
+        """
+        def query_endpoint():
+            url = '{ep}/v1/reports/{re}'.format(ep=self.endpoint, re=report)
+            headers = {}
+            if self.token:
+                headers['X-Auth-Token'] = self.token
+            r = requests.get(url, headers=headers)
+            r.raise_for_status()
+            return r.json()
+    
+        if self.cache:
+            path = './{report}.json'.format(report=report)
+            try:
+                with open(path, 'r') as f:
+                    return json.loads(f.read())
+            except IOError:
+                data = query_endpoint()
+                with open(path, 'w') as f:
+                    f.write(json.dumps(data))
+                return data
+    
+        return query_endpoint()
 
-    if cache:
-        path = './{report}.json'.format(report=report)
+    def fetch_w(self, report):
+        """
+        Wrapper for `fetch`, incorporating commandline arguments and a little
+        bit of error handling to make users' lives easier.
+        """
+        if args.debug:
+            print 'Fetching "{}"...'.format(report)
         try:
-            with open(path, 'r') as f:
-                return json.loads(f.read())
-        except IOError:
-            data = query_endpoint()
-            with open(path, 'w') as f:
-                f.write(json.dumps(data))
+            data = self.fetch(report)
+            if args.debug:
+                print 'Fetched "{}".'.format(report)
             return data
+        except requests.exceptions.HTTPError as ex:
+            if ex.response.status_code == 401 and not args.token:
+                # it's easy to forget to set OS_TOKEN
+                print >> sys.stderr, 'Hint: maybe you need to set OS_TOKEN.'
+            raise
 
-    return query_endpoint()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -57,28 +82,12 @@ if __name__ == '__main__':
         except KeyError:
             pass  # hope that reporting-api endpoint does not require token
 
-    def fetch_w(report):
-        """
-        Wrapper for `fetch`, incorporating commandline arguments and a little
-        bit of error handling to make users' lives easier.
-        """
-        if args.debug:
-            print 'Fetching "{}"...'.format(report)
-        try:
-            data = fetch(report, args.endpoint, token=args.token, cache=args.cache)
-            if args.debug:
-                print 'Fetched "{}".'.format(report)
-            return data
-        except requests.exceptions.HTTPError as ex:
-            if ex.response.status_code == 401 and not args.token:
-                # it's easy to forget to set OS_TOKEN
-                print >> sys.stderr, 'Hint: maybe you need to set OS_TOKEN.'
-            raise
+    client = ReportingClient(**vars(args))
 
     # grab all the required data
-    hypervisor = fetch_w('hypervisor')
-    instance = fetch_w('instance?active=1')
-    project = fetch_w('project')
+    hypervisor = client.fetch_w('hypervisor')
+    instance = client.fetch_w('instance?active=1')
+    project = client.fetch_w('project')
 
     # check that every hypervisor has availability_zone defined
     # (since that's what we'll be using to determine AZ for each instance)
